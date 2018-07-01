3b1e5564a1c70123f0c9dde841d043e760c9b45a
Mathew Thomas <mathew.thomas@longdivision.com>
2018-07-01 16:07:23 -0400

    body parser
    Signed-off-by: Mathew Thomas <mathew.thomas@longdivision.com>

---
+++ post_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# docker run -p 3000:3000 -p 3001:3001 -v "$(pwd)"/src:/usr/src/app/src -d  mthomas/node-api
+
+# curl -XPOST http://0.0.0.0:8888/api/reviews -d @mydata.txti
+
+# curl -v -XPOST -H "Accept: application/json" -d {"first":"bob"} --url http://0.0.0.0:8888/api/reviews 
+curl -v  -H "Content-Type: application/json" -d '{"id":"wrwoeuoweirwer"}' --url http://0.0.0.0:8888/api/reviews 
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const os = require('os');
 const notes = require('./notes.js');
 const _ = require('lodash');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // Constants
 const PORT = 8888;
@@ -11,9 +12,31 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-  res.send(`Hello world\n\n`);
+  res.send(`Hello node\n\n`);
 });
+
+app.post('/api/reviews', (req, res) => {
+  console.log(`request headers: ${req}`);
+  for (var key in req) {
+    console.log(key);
+  }
+
+  console.log(req.hostname);
+  console.log(req.route);
+  console.log(req.path);
+  console.log(req.body);
+
+  // console.log("now the response object...\n\n\n\n");
+  // for (var key in res) {
+  //   console.log(key);
+  // }
+
+
+  res.send('reviews post');
+})
 
 let user = os.userInfo();
 console.log(user);
